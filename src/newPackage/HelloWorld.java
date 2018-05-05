d0504b86456365bd56cbfd7375356a6cbc024b2d
RKSuman <sumn.rk@gmail.com>
2018-05-05 12:02:18 +0530

    Commited Simple Java Programs folder

---
+++ src/newPackage/HelloWorld.java
@@ -0,0 +1,16 @@
+package newPackage;
+
+
+/**
+ * @author Suman R K
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	   System.out.println("jai Ganesha");
+
+	}
+
+}
